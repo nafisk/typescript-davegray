527ce34863ada75b1788744806597511f652d693
nafisk <nafisrizwank@gmail.com>
2024-03-17 15:42:56 -0400

    tutorial 2

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,21 @@
 "use strict";
-let username = 'Nafis';
-console.log(username);
-let a = 12;
-let b = '6';
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+// let album: string | number;
+let album;
+myName = 'David';
+meaningOfLife = 42;
+isLoading = true;
+// This is allowed because of the `any` type
+album = 'Nafis';
+album = 42;
+album = true;
+// plus can be used for both arithmetic addition and string concatenation
+// therefore the types are important
+const sum = (a, b) => a + b;
+// union types
+let postId;
+let isActive;
+// Regex in TypeScript
+let re = /\w+/g;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-let username = 'Nafis';
-console.log(username);
+let myName: string = 'Dave';
+let meaningOfLife: number;
+let isLoading: boolean;
+// let album: string | number;
+let album: any;
 
-let a: number = 12;
-let b: string = '6';
-let c: number = 2;
+myName = 'David';
+meaningOfLife = 42;
+isLoading = true;
 
-console.log(a / b);
+// This is allowed because of the `any` type
+album = 'Nafis';
+album = 42;
+album = true;
 
-console.log(c * b);
+// plus can be used for both arithmetic addition and string concatenation
+// therefore the types are important
+const sum = (a: number, b: string) => a + b;
+
+// union types
+let postId: string | number;
+let isActive: number | boolean;
+
+// Regex in TypeScript
+let re: RegExp = /\w+/g;
