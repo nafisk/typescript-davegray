8d69603fd967da1e1bac7faf95a3d87d381a0847
nafisk <nafisrizwank@gmail.com>
2024-03-19 15:43:31 -0400

    tutorial 4

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,38 @@
 "use strict";
-let myName = 'Dave';
-let meaningOfLife;
-let isLoading;
-// let album: string | number;
-let album;
-myName = 'David';
-meaningOfLife = 42;
-isLoading = true;
-// This is allowed because of the `any` type
-album = 'Nafis';
-album = 42;
-album = true;
-// plus can be used for both arithmetic addition and string concatenation
-// therefore the types are important
-const sum = (a, b) => a + b;
-// union types
-let postId;
-let isActive;
-// Regex in TypeScript
-let re = /\w+/g;
+// interface PostId = stringOrNumber;
+// Literal types - can only be assigned to a specific values
+let myName;
+// myName = 'John'; // Error
+let userName;
+userName = 'John';
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (msg) => {
+    console.log(msg);
+};
+logMsg('Hello');
+logMsg(add(1, 2));
+// logMsg(add('1', 1)); // Error
+let subtract = function (a, b) {
+    return a - b;
+};
+let multiply = (a, b) => a * b;
+let multiplyInt = (a, b) => a * b;
+// Optional parameters
+const addAll = (a, b, c) => {
+    return a + b + (c || 0);
+};
+// Default parameters
+const sumAll = (a, b, c = 0) => {
+    return a + b + c;
+};
+// Rest parameters
+const total = (...nums) => {
+    return nums.reduce((a, b) => a + b);
+};
+console.log(total(1, 2, 3, 4, 5));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,68 @@
-let myName: string = 'Dave';
-let meaningOfLife: number;
-let isLoading: boolean;
-// let album: string | number;
-let album: any;
+// Type Aliases
+type stringOrNumber = string | number;
 
-myName = 'David';
-meaningOfLife = 42;
-isLoading = true;
+type stringOrNumberArray = (string | number)[];
 
-// This is allowed because of the `any` type
-album = 'Nafis';
-album = 42;
-album = true;
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+};
 
-// plus can be used for both arithmetic addition and string concatenation
-// therefore the types are important
-const sum = (a: number, b: string) => a + b;
+// assigning types to other types is only allowed in type aliases not interfaces
+// user interfaces for only objects and classes
+type UserId = stringOrNumber;
+// interface PostId = stringOrNumber;
 
-// union types
-let postId: string | number;
-let isActive: number | boolean;
+// Literal types - can only be assigned to a specific values
+let myName: 'Dave';
+// myName = 'John'; // Error
+let userName: 'Dave' | 'John' | 'Jane';
+userName = 'John';
 
-// Regex in TypeScript
-let re: RegExp = /\w+/g;
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logMsg = (msg: any): void => {
+  console.log(msg);
+};
+
+logMsg('Hello');
+logMsg(add(1, 2));
+// logMsg(add('1', 1)); // Error
+
+let subtract = function (a: number, b: number): number {
+  return a - b;
+};
+
+// using types vs interface
+type mathFunction = (a: number, b: number) => number;
+let multiply: mathFunction = (a, b) => a * b;
+
+interface mathFunctionInt {
+  (a: number, b: number): number;
+}
+let multiplyInt: mathFunction = (a, b) => a * b;
+
+// Optional parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  return a + b + (c || 0);
+};
+
+// Default parameters
+const sumAll = (a: number, b: number, c: number = 0): number => {
+  return a + b + c;
+};
+
+// Rest parameters
+const total = (...nums: number[]): number => {
+  return nums.reduce((a, b) => a + b);
+};
+
+console.log(total(1, 2, 3, 4, 5));
+
+const createError = (errMsg: string): never => {
+  throw new Error(errMsg);
+};
